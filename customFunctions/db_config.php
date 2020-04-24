0e970318ca3f1aa876c7894b6a00a9e92aadee93
DoychinBorisov84 <doychinborisov84@gmail.com>
2020-04-24 17:03:27 +0300

    Added the default db_config

---
+++ customFunctions/db_config.php
@@ -0,0 +1,23 @@
+<?php
+
+// Credentials for the Database
+$username = 'root';
+$password = 'root!Admin1';
+$database = 'single_page_application';
+$host = 'localhost';
+$dsn = "mysql:host=$host;dbname=$database";
+$charset = 'utf8mb4';
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try{
+	$connection = new PDO($dsn, $username, $password, $options);	
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
+
+// $connection = null;
